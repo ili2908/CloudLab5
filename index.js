983142b50a2e52f1c0358149e67c5e50f66e3bf2
IliaKova <ilia.kovalenko@remme.io>
2023-03-26 18:09:50 +0300

    testing Linter

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const PORT = 80;
+
+http.createServer((_, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Ok');
+}).listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`);
+});
